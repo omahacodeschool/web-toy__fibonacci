f5e6c150c38cee0a578b68b7aaf98a8416b81cbf
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-02-23 14:12:05 -0600

    edits files to set up instructions for fibonacci project

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,12 @@
 # This controller action should have a form for the user
-# to enter some Fahrenheit value.
+# to enter some integer.
 MyApp.get "/" do
   erb :"homepage"
 end
 
 # This is the action that the form should submit to.
-# It does the calculation and returns a view that
-# displays the converted-to-Celsius temperature.
-MyApp.get "/celsius" do
-  erb :"to_celsius"
+# It does the calculation and returns the Fibonacci sequence value
+# at the index provided by the user.
+MyApp.get "/fibonacci_result" do
+  erb :"fibonacci_result"
 end
